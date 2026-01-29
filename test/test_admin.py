3f5949cbb7c60a23b780c70482e5abbf920632be
= <=>
2026-01-29 19:37:43 -0300

    test admin updated

--- test/test_admin.py
+++ test/test_admin.py
@@ -1,6 +1,7 @@
 from .utils import *
 from ..routers.admin import get_db, get_current_user
 from fastapi import status
+from ..models import Todos
 
 app.dependency_overrides[get_db] = override_get_db    
 app.dependency_overrides[get_current_user] = override_get_current_user
@@ -10,3 +11,16 @@ def test_admin_read_all_authenticated(test_todo):
     response = client.get("/admin/todo")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == [{'title': 'Test Todo', 'description': 'Test Description', 'priority': 1, 'id': 1, 'complete': False,  'owner_id': 1}]
+
+def test_admin_delete_todo(test_todo):
+    response = client.delete("/admin/todo/1")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 1).first()
+    assert model is None
+
+def test_admin_delete_todo_not_found(test_todo):
+    response = client.delete("/admin/todo/2")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {'detail': 'Todo not found'}
